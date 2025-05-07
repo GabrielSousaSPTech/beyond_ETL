f2dcf08b1c4302afd35b0ae756f24adfe44600dc
GabrielSousaSPTech <gabriel.sousa@sptech.school>
2025-05-07 00:38:47 -0300

    Alteração no tipo de credencial da AWS

--- ETLbba/src/main/java/bbaETL/BucketAWS.java
+++ ETLbba/src/main/java/bbaETL/BucketAWS.java
@@ -25,7 +25,7 @@ public class BucketAWS {
         this.bucketRegion = Region.US_EAST_1;
         this.client = S3Client.builder()
                 .region(bucketRegion)
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(ProfileCredentialsProvider.create())
                 .build();
         log = new LogDao(env);
     }
